c3fb1853f57f7c7c3bb48a426b7b6ae93d2b282d
martinovandra11 <martin18ti@mahasiswa.pcr.ac.id>
2021-04-24 01:51:28 +0700

    perbaikan label

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -2,7 +2,7 @@
 <div class="main-content">
     <section class="section">
     <div class="section-header">
-        <h1>Trip Bulan Ini</h1>
+        <h1>Trip Hari Ini</h1>
     </div>
 
     <div class="row">
