70111ac326403a10b47c656a3ab7791fe6fb3333
raihanpcr <52515421+raihanpcr@users.noreply.github.com>
2021-04-23 23:31:34 +0700

    ini update

--- application/views/v_dashboard2.php
+++ application/views/v_dashboard2.php
@@ -68,15 +68,69 @@
 					<h3>Laporan Bongkar Bulan Ini</h3>
 			</div>
 		</div>
+		
 		<div class="row justify-content-center">
-               <div class="col-6 col-sm-6 col-md-12">
+               <div class="col-6 col-sm-6 col-md-6">
 				<div class="card card-statistic-1">
 					<div class="card-icon bg-success">
+						<i class="fas fa-stopwatch"></i>
+					</div>
+					<div class="card-wrap">
+						<div class="card-header">
+							<h4>Waktu Bongkar kurang 20 Menit</h4>
+						</div>
+						<div class="card-body">
+                                   <?php 
+                                        foreach($hariini as $ini) { 
+                                   ?>
+                                   
+                                   <?= number_format($ini['JumlahPerHari'], 0,',','.');?>
+                                   
+                                   <?php } ?>
+						</div>
+                              <div>
+                                   <a href="<?= base_url('c_dashboard') ?>" >Lihat Data</a>
+                              </div>
+                              
+					</div>
+				</div>
+			</div>
+
+               <div class="col-6 col-md-6">
+				<div class="card card-statistic-1">
+					<div class="card-icon bg-danger">
 						<i class="fas fa-shipping-fast"></i>
 					</div>
 					<div class="card-wrap">
 						<div class="card-header">
-							<h4>Trip Hari Ini</h4>
+							<h4>Muatan Lebih dari 5 TON</h4>
+						</div>
+						<div class="card-body">
+                                   <?php 
+                                        foreach($bulanini as $ini) { 
+                                   ?>
+                                   
+                                        <?= number_format($ini['JumlahPerBulan'], 0,',','.');?>
+                                   
+                                   <?php } ?>
+						</div>
+                              <div>
+                                   <a href="<?= base_url('c_bulanini') ?>">Lihat Data</a>
+                              </div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row justify-content-center">
+               <div class="col-6 col-sm-6 col-md-12">
+				<div class="card card-statistic-1">
+					<div class="card-icon bg-primary">
+						<i class="fas fa-shipping-fast"></i>
+					</div>
+					<div class="card-wrap">
+						<div class="card-header">
+							<h4>Bongkar < 20 Menit Dan Kapasitas > 5 Ton</h4>
 						</div>
 						<div class="card-body">
                                    <?php 
@@ -95,4 +149,6 @@
 				</div>
 			</div>
 		</div>
+
+		
 </div>
